276c5af7ddf6ac14a47d5bb2129c7838c8fb75b3
sohosoki <so.hosoki@enechange.co.jp>
2024-01-18 19:28:57 +0900

    create controller

--- app/routers/plan.rb
+++ app/routers/plan.rb
@@ -1,35 +1,10 @@
 require_relative 'base'
-require_relative '../helpers/json'
-require_relative '../usecases/plan/index'
+require_relative '../controllers/plan'
    
 module Router
   class Plan < Router::Base
     get '/plans' do
-      response_format = {
-        include: {
-          plans: {
-            only: ['code', 'name'],
-            include: {
-              demand_charges: {
-                only: ['ampere_from', 'ampere_to', 'charge']
-              },
-              energy_charges: {
-                only: ['kwh_from', 'kwh_to', 'rate']
-              }
-            }
-          }
-        }
-      }
-      usecase = UseCase::Plan::Index.new
-      plans = usecase.execute
-      ok(
-        Helper::Json.from(
-          {
-            plans: plans
-          }, 
-          response_format
-        )
-      )
+      Controller::Plan.new.index
     end
   end
 end
